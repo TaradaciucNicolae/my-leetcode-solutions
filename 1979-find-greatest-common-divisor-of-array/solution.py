479281c504d172165aae1baa52997e8a2962c29d
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-07-18 16:57:16 +0300

    Runtime: 0 ms (100.00%), Memory: 19.4 MB (11.34%) - Python - LeetHub Auto Commit

--- 1979-find-greatest-common-divisor-of-array/solution.py
+++ 1979-find-greatest-common-divisor-of-array/solution.py
@@ -1,16 +1,16 @@
-class Solution:
-    def findGCD(self, nums: List[int]) -> int:
-        
-        smallest = min(nums)
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        
+        smallest = min(nums)
 
-        largest = max(nums)
+        largest = max(nums)
 
-        reversed_counter = smallest
+        reversed_counter = smallest
 
-        while reversed_counter != 0:
+        while reversed_counter != 0:
 
-            if smallest % reversed_counter == 0 and largest % reversed_counter == 0:
-                return reversed_counter
+            if smallest % reversed_counter == 0 and largest % reversed_counter == 0:
+                return reversed_counter
 
 
-            reversed_counter -= 1
+            reversed_counter -= 1
