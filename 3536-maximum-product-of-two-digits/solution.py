c45368581cb5b5996db965364ce06e82f2c022e1
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-07-25 17:33:56 +0300

    Runtime: 0 ms (100.00%), Memory: 19.2 MB (54.97%) - Python - SafeLeetSync Auto Commit

--- 3536-maximum-product-of-two-digits/solution.py
+++ 3536-maximum-product-of-two-digits/solution.py
@@ -1,18 +1,41 @@
 class Solution:
     def maxProduct(self, n: int) -> int:
         
-        max_pair = 0
+        # max_pair = 0
 
-        for i, each_1 in enumerate(str(n)):
+        # for i, each_1 in enumerate(str(n)):
 
-            val_1 = int(each_1)
+        #     val_1 = int(each_1)
 
-            for j, each_2 in enumerate(str(n)):
+        #     for j, each_2 in enumerate(str(n)):
 
-                val_2 = int(each_2)
+        #         val_2 = int(each_2)
 
-                if i != j:
-                    max_pair = max(max_pair, val_1 * val_2)
+        #         if i != j:
+        #             max_pair = max(max_pair, val_1 * val_2)
 
         
-        return max_pair
+        # return max_pair
+
+
+
+        # Because it's a pair, we can search for the 2 biggest digits
+
+
+        first = 0
+        second = 0
+
+        while n > 0:
+
+            digit = n % 10
+
+            if digit >= first:
+                second = first
+                first = digit   
+
+            elif digit > second:
+                second = digit
+
+            n  =  n // 10
+
+        return first*second
