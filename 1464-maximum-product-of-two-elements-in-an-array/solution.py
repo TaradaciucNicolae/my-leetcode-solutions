f57d45676d0241e150db163558e7f9111d8d3e72
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-07-27 21:34:42 +0300

    Runtime: 0 ms (100.00%), Memory: 19.2 MB (92.40%) - Python - SafeLeetSync Auto Commit

--- 1464-maximum-product-of-two-elements-in-an-array/solution.py
+++ 1464-maximum-product-of-two-elements-in-an-array/solution.py
@@ -1,11 +1,30 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
-        n=len(nums)
-        max_comb=0
+        # n=len(nums)
+        # max_comb=0
 
-        for i in range(0,n):
-            for j in range(i+1,n):
-                max_comb = max(max_comb, (nums[i] - 1) * (nums[j] - 1) )
+        # for i in range(0,n):
+        #     for j in range(i+1,n):
+        #         max_comb = max(max_comb, (nums[i] - 1) * (nums[j] - 1) )
         
-        return max_comb
+        # return max_comb
+
+
+        biggest = 0
+        second_biggest = 0
+
+        for val in nums:
+
+            if val >= biggest:
+
+                second_biggest = biggest
+
+                biggest = val
+            
+            elif val < biggest and val > second_biggest:
+
+                second_biggest = val
+
+        
+        return (biggest -1) * ( second_biggest -1)
