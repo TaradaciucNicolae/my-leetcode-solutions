db87be93479b5f67460d4ac6468db9f8561b9dea
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-07-30 10:33:55 +0300

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (16.35%) - Python - SafeLeetSync Auto Commit

--- 3014-minimum-number-of-pushes-to-type-word-i/solution.py
+++ 3014-minimum-number-of-pushes-to-type-word-i/solution.py
@@ -1,21 +1,34 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        
+
         n = len(word)
-        print(n)
-
+        count = n // 8
         total = 0
-        count = 1
 
-        while n != 0:
+        for i in range(1, count+1):
 
-            if n > 8:
-                total  = total + 8 * count
-                n -= 8
-                count  += 1
-                
-            elif n <= 8:
-                total = total + n * count
-                n = 0
-        
+            total += i*8
+
+        total += (count+1) * (n % 8)
+
         return total
+
+
+        # n = len(word)
+        # print(n)
+
+        # total = 0
+        # count = 1
+
+        # while n != 0:
+
+        #     if n > 8:
+        #         total  = total + 8 * count
+        #         n -= 8
+        #         count  += 1
+                
+        #     elif n <= 8:
+        #         total = total + n * count
+        #         n = 0
+        
+        # return total
