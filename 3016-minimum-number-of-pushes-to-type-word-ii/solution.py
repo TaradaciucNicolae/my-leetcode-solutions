9fa749fb5fa171112c5b7f50b2bb20a17fa10358
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-07-31 12:37:58 +0300

    Runtime: 174 ms (39.80%), Memory: 20 MB (48.16%) - Python - SafeLeetSync Auto Commit

--- 3016-minimum-number-of-pushes-to-type-word-ii/solution.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/solution.py
@@ -1,29 +1,56 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        
+
+
         total_cost = 0
         count_in_map = defaultdict(int)
 
-        for i, each_ch in enumerate(word):
+        for each_ch in word:
             count_in_map[each_ch] +=1
 
         # Sorting
-        count_in_map = dict(sorted(count_in_map.items(),  key=lambda item : item[1] ,reverse = True))
+        freqs = sorted(count_in_map.values(), reverse = True)
 
-        n = len(count_in_map)
 
         counter =0
         multiplier = 1
 
-        for each_char in count_in_map:
+        for freq in freqs:
 
             if counter == 8:
                 multiplier +=1
                 counter = 0
 
-            total_cost += count_in_map[each_char] * multiplier
-
+            total_cost += freq * multiplier
             counter +=1
 
-
         return total_cost
+
+
+
+
+
+        # total_cost = 0
+        # count_in_map = defaultdict(int)
+
+        # for each_ch in word:
+        #     count_in_map[each_ch] +=1
+
+        # # Sorting
+        # count_in_map = dict(sorted(count_in_map.items(),  key=lambda item : item[1] ,reverse = True))
+
+        # counter =0
+        # multiplier = 1
+
+        # for each_char in count_in_map:
+
+        #     if counter == 8:
+        #         multiplier +=1
+        #         counter = 0
+
+        #     total_cost += count_in_map[each_char] * multiplier
+
+        #     counter +=1
+
+
+        # return total_cost
