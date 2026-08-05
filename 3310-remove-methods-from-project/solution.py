ff8d68e1def6027ec7c745a0fd56ea938c255ce2
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-08-05 13:59:51 +0300

    Runtime: 255 ms (81.06%), Memory: 106.9 MB (59.09%) - Python - SafeLeetSync Auto Commit

--- 3310-remove-methods-from-project/solution.py
+++ 3310-remove-methods-from-project/solution.py
@@ -4,7 +4,7 @@ class Solution:
         for a, b in invocations:
             nodes[a].append(b)
 
-        # 1. reachable from k
+        #  reachable from k
         visited = {k}
         stack = [k]
         while stack:
@@ -14,7 +14,7 @@ class Solution:
                     visited.add(nxt)
                     stack.append(nxt)
 
-        # Check if any node outside the group invokes into the group
+        # check if any node outside the group invokes into the group
         removable = True
         for a, b in invocations:
             if b in visited and a not in visited:
@@ -24,13 +24,13 @@ class Solution:
         output=[]    
         
             
-        # If it can't be removed -> return all nodes unchanged
+        # if it can't be removed -> return all nodes unchanged
         if not removable:
             output = list(range(0,n))
             return output
         else: 
             
-            # Otherwise, return only the non-suspicious methods
+            #  return only the non-suspicious methods
             for i in range(0,n):
                 if i not in visited:
                     output.append(i)
