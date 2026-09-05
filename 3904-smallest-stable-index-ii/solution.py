7c5824a32066546ff2df154d5d53e5cac0b3209f
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-09-05 10:08:11 +0300

    Runtime: 147 ms (80.46%), Memory: 33.2 MB (23.64%) - Python - SafeLeetSync Auto Commit

---
+++ 3904-smallest-stable-index-ii/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        # mn[i] = minimum from nums[i:] (sufix)
+        mn = [0] * n
+        mn[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1, -1):
+            mn[i] = min(nums[i], mn[i + 1])
+
+        # mx = max from nums[0:i+1] (prefix)
+        mx = float('-inf')
+        for i in range(n):
+            mx = max(mx, nums[i])
+            if mx - mn[i] <= k:
+                return i
+
+        return -1
