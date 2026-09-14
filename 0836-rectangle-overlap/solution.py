653b7ce7915371f00fed9e6d09233090e545a3f4
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-09-14 10:12:44 +0300

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (19.53%) - Python - SafeLeetSync Auto Commit

---
+++ 0836-rectangle-overlap/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        r1x1,r1y1,r1x2,r1y2 = rec1
+        r2x1,r2y1,r2x2,r2y2 = rec2
+
+        if r2x2 <= r1x1 or r1x2 <= r2x1 or r2y2 <= r1y1 or r1y2 <= r2y1:
+            return False
+        
+        return True
