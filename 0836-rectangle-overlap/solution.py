0756592378e0132f6e55b1b45a6144ba21c14caa
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-09-19 09:46:38 +0300

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (60.08%) - Python - SafeLeetSync Auto Commit

--- 0836-rectangle-overlap/solution.py
+++ 0836-rectangle-overlap/solution.py
@@ -3,7 +3,4 @@ class Solution:
         r1x1,r1y1,r1x2,r1y2 = rec1
         r2x1,r2y1,r2x2,r2y2 = rec2
 
-        if r2x2 <= r1x1 or r1x2 <= r2x1 or r2y2 <= r1y1 or r1y2 <= r2y1:
-            return False
-        
-        return True
+        return r1x1 < r2x2 and r2x1 < r1x2 and r1y1 < r2y2 and r2y1 < r1y2
